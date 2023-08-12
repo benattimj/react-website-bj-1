07f45e122ae84c9ff193c95721aaab8363c7aa81
Murilo Benatti <benatti1104@gmail.com>
2023-08-12 18:03:34 -0300

    Update

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -6,7 +6,7 @@ import './HeroSection.css';
 function HeroSection() {
     return (
         <div className='hero-container'>
-            <video src='../videos/video-1.mp4' autoPlay loop muted />
+            <video src='../videos/video-2.mp4' autoPlay loop muted />
             <h1>BJSYS Hotel Managements</h1>
         
             <p>Otimize  seus sistemas </p>
