64add2e5fcee12a6f2434bc300b997b9a58057bd
Murilo Benatti <benatti1104@gmail.com>
2023-08-24 16:20:33 -0300

    Retirei o Contato de todas outras pages

--- src/App.js
+++ src/App.js
@@ -20,12 +20,12 @@ function App() {
           <Route path='/' exact component={Home} />
           <Route path='/services' component={Services} />
           <Route path='/products' component={Products} />
-          <Route path='/sign-up' component={SignUp} />
-          
-          
-
+          <Route path='/sign-up' component={SignUp} />       
+           <Route path='/Sobre' component={Contact} />
+                    
+                    
         </Switch>
-<Contact></Contact>
+
         <Footer/>
       </Router>
 
---
+++ src/components/dashboard/Main.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../../App.css';
+import CardItem from '../CardItem';
+
+
+function Main() {
+  return (
+    <>
+       
+     
+    </>
+  );
+}
+
+export default Main;
